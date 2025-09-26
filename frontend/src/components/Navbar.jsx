f15911c864ca0528d88392a57d406e8bc49eca15
DarkPlusX <158550285+DarkPlus2@users.noreply.github.com>
2025-09-26 09:28:38 +0600

    Delete frontend/src/components/Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="bg-gray-900 text-white p-4 flex justify-between">
-      <h1 className="text-2xl font-bold">AnimeNest</h1>
-    </nav>
-  );
-}
