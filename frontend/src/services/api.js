58a893a564fee653e1a4db5075c7675d23174f4f
DarkPlusX <158550285+DarkPlus2@users.noreply.github.com>
2025-09-26 09:28:50 +0600

    Delete frontend/src/services/api.js

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api";
-
-export const getAnimeList = (page=1) => axios.get(`${API_URL}/anime?page=${page}`);
-export const getAnimeDetail = (id) => axios.get(`${API_URL}/anime/${id}`);
-export const getWatchlist = (email) => axios.get(`${API_URL}/watchlist/${email}`);
-export const addToWatchlist = (email, animeId) => axios.post(`${API_URL}/watchlist/add`, { email, animeId });
